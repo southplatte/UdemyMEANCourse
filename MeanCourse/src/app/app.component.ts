d01bfa0ac891df2bb5147832645ab590850f9646
Bill N <bnab@southplattewebdesign.com>
2024-07-03 17:02:25 -0500

    lesson 21 adding header and diving deeper into angular structure

--- MeanCourse/src/app/app.component.ts
+++ MeanCourse/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { FormsModule } from '@angular/forms';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostCreateComponent, FormsModule],
+  imports: [RouterOutlet, PostCreateComponent, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
