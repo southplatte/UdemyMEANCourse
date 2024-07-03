c1799b450ca8e5d71f61aac6bacd002da12b89b1
Bill N <bnab@southplattewebdesign.com>
2024-07-03 16:03:17 -0500

    end of lesson 20 adding angular material for styling

--- MeanCourse/src/app/app.config.ts
+++ MeanCourse/src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
 };
--- MeanCourse/src/app/posts/post-create/post-create.component.ts
+++ MeanCourse/src/app/posts/post-create/post-create.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-post-create',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css'
 })
