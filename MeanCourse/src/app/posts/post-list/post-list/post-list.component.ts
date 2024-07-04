4479b5c2cc78ba7ae2f30598ebbf9e5b78ce35e6
Bill N <bnab@southplattewebdesign.com>
2024-07-04 14:10:33 -0500

    lesson 23 adding conditional ng directives into html templates

--- MeanCourse/src/app/posts/post-list/post-list/post-list.component.ts
+++ MeanCourse/src/app/posts/post-list/post-list/post-list.component.ts
@@ -4,21 +4,24 @@ import { MatAccordion } from '@angular/material/expansion';
 import { MatExpansionPanelHeader } from '@angular/material/expansion';
 import { NgFor } from '@angular/common';
 import { NgForOf } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [MatExpansionPanel, MatAccordion, MatExpansionPanelHeader, NgFor, NgForOf],
+  imports: [MatExpansionPanel, MatAccordion, MatExpansionPanelHeader, NgFor, NgForOf, NgIf],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
-  posts = [
-    {title: 'First Post', content: 'Some content for first post'},
-    {title: 'Second Post', content: 'Some content for second post'},
-    {title: 'Third Post', content: 'Some content for third post'},
-    {title: 'Fourth Post', content: 'Some content for fourth post'},
+  // posts = [
+  //   {title: 'First Post', content: 'Some content for first post'},
+  //   {title: 'Second Post', content: 'Some content for second post'},
+  //   {title: 'Third Post', content: 'Some content for third post'},
+  //   {title: 'Fourth Post', content: 'Some content for fourth post'},
 
-  ]
+  // ]
+
+  posts = []
 
 }
