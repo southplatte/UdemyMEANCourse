b3ce9f9c3212fabbf72f532f986c9a60349309b0
Bill N <bnab@southplattewebdesign.com>
2024-07-08 15:55:43 -0500

    lesson 26 adding error handling to forms

--- MeanCourse/src/app/posts/post-create/post-create.component.ts
+++ MeanCourse/src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatError } from '@angular/material/input';
+import { NgIf } from '@angular/common';
 
 //do not include these in the component imports
 //import { EventEmitter } from '@angular/core';
@@ -14,7 +16,7 @@ import { Post } from '../posts.model';
 @Component({
   selector: 'app-post-create',
   standalone: true,
-  imports: [FormsModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule],
+  imports: [FormsModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, MatError, NgIf],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css'
 
@@ -22,15 +24,18 @@ import { Post } from '../posts.model';
 export class PostCreateComponent {
   createPostMsg = 'Create New Post: ';
   enteredTitle = '';
-  enteredContent = '';
+  content = '';
   @Output() postCreated = new EventEmitter<Post>();
   
   newPost = 'NO CONTENT';
-  onAddPost(){
+  onAddPost(form: NgForm){
+    if(form.invalid){
+      return;
+    }
     //this.newPost = this.enteredValue;
     const post: Post = {
-      title: this.enteredTitle, 
-      content: this.enteredContent
+      title: form.value.title, 
+      content: form.value.content
     }
     this.postCreated.emit(post);
   }
