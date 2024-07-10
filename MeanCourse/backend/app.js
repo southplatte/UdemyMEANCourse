365db3166f70a3de907d33784d1af0fe9fab782f
Bill N <bnab@southplattewebdesign.com>
2024-07-10 13:12:12 -0500

    lesson 38 adding static posts to serve with node

--- MeanCourse/backend/app.js
+++ MeanCourse/backend/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware layer');
-    next();
-});
 
-app.use((req, res, next) => {
-    res.send('Hello from express updated server.js');
+app.use('/api/posts/', (req, res, next) => {
+    const posts = [
+        { 
+            id: '8736post', 
+            title: 'First server Side post', 
+            content: 'This is post content from the server'
+        },
+        {
+            id: '3456hpost',
+            title: 'Second server side post',
+            content: 'this is post content for post 2 from server'
+        }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: posts
+    });
 });
 
 module.exports = app;
