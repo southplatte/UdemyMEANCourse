be1f0509dfff65999a24a670b30c2438156d1afe
Bill N <bnab@southplattewebdesign.com>
2024-08-01 15:21:20 -0500

    lesson 55 end - retrieving data from mongoDB

--- MeanCourse/backend/app.js
+++ MeanCourse/backend/app.js
@@ -38,22 +38,15 @@ app.post('/api/posts', (req, res, next) => {
 
 
 app.get('/api/posts/', (req, res, next) => {
-    const posts = [
-        { 
-            id: '8736post', 
-            title: 'First server Side post', 
-            content: 'This is post content from the server and it\'s amazing.....'
-        },
-        {
-            id: '3456hpost',
-            title: 'Second server side post',
-            content: 'this is post content for post 2 from server and it will be even better....and then some'
-        }
-    ];
-    res.status(200).json({
-        message: 'Posts fetched successfully',
-        posts: posts
+    Post.find()
+    .then(documents => {
+        console.log(documents);
+        res.status(200).json({
+            message: 'Posts fetched successfully',
+            posts: documents
+        });
     });
+    
 });
 
 module.exports = app;
